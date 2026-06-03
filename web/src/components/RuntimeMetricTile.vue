b4f499048b58351dbf1e22ee92e8a7dd85f24e99
Calvin1989 <1400235935@qq.com>
2026-06-03 16:19:27 +0800

    ui: extract runtime metric tile component

---
+++ web/src/components/RuntimeMetricTile.vue
@@ -0,0 +1,20 @@
+<script setup>
+defineProps({
+  label: {
+    type: String,
+    required: true,
+  },
+  value: {
+    type: [String, Number],
+    required: false,
+    default: "",
+  },
+});
+</script>
+
+<template>
+  <div class="hero-metric-item">
+    <span class="runtime-label">{{ label }}</span>
+    <strong class="runtime-value">{{ value }}</strong>
+  </div>
+</template>
